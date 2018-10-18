820760a6d7aa43d01c9605e63847ee23da4c63d7
rrusson <Ryan.Russon@mobilehelp.com>
2018-10-17 20:13:46 -0400

    Add project files.

---
+++ GrammarExtensionsTests/IndefiniteArticleTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GrammarExtensions;
+
+namespace GrammarExtensionsTests
+{
+    [TestClass]
+    public class IndefiniteArticleTests
+    {
+        [TestMethod()]
+        public void IndefiniteArticle_aTest()
+        {
+            var aWords = new string[] {
+                "banana",
+                "ewe",
+                "European",
+                "one",
+                "once",
+                "one-armed",
+                "one-time",
+                "ouija",
+                "ubiquity",
+                "uboat",
+                "ufo",
+                "UK",
+                "ukulele",
+                "u.n.",
+                "unanimous",
+                "unicorn",
+                "unilateral",
+                "urinologist",
+                "urea",
+                "useful",
+                "user"
+            };
+
+            foreach (var word in aWords)
+            {
+                if (word.IndefiniteArticle() == "an")
+                {
+                    Assert.Fail($"Wrong IndefiniteArticle for {word}");
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void IndefiniteArticle_anTest()
+        {
+            var anWords = new string[]
+            {
+                "apple",
+                "euler",
+                "honor",
+                "honorable",
+                "hour",
+                "honest",
+                "heir",
+                "onerous",
+                "Oaxaca",
+                "unassailable",
+                "ubermensch",
+                "unanswered",
+                "ungrammatical",
+                "unilluminated",
+                "unintentional",
+                "unlikely",
+                "urgent",
+                "utter"
+            };
+
+            foreach (var word in anWords)
+            {
+                if (word.IndefiniteArticle() == "a")
+                {
+                    Assert.Fail($"Wrong IndefiniteArticle for {word}");
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void IndefiniteArticle_caseSensitivityTest()
+        {
+            Assert.AreEqual("a", "hamburger".IndefiniteArticle());
+            Assert.AreEqual("a", "Hamburger".IndefiniteArticle());
+
+            Assert.AreEqual("an", "honor".IndefiniteArticle());
+            Assert.AreEqual("an", "Honor".IndefiniteArticle());
+
+            //Acronyms
+            Assert.AreEqual("a", "Faa".IndefiniteArticle());
+            Assert.AreEqual("an", "FAA".IndefiniteArticle());
+        }
+
+        [TestMethod()]
+        public void IndefiniteArticle_emptyTest()
+        {
+            string nothing = null;
+            Assert.AreEqual("", nothing.IndefiniteArticle());
+            Assert.AreEqual("", "".IndefiniteArticle());
+            Assert.AreEqual("", "       ".IndefiniteArticle());
+        }
+
+    }
+}
---
+++ GrammarExtensionsTests/ToPluralTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity.Design.PluralizationServices;
+using System.Globalization;
+using GrammarExtensions;
+
+namespace GrammarExtensionsTests
+{
+    [TestClass]
+    public class ToPluralTests
+    {
+        [TestMethod()]
+        public void Leave_it_to_the_professionals_test()
+        {
+            var x = PluralizationService.CreateService(CultureInfo.CurrentCulture);
+            string mice = x.Pluralize("mouse");
+            string potatoes = x.Pluralize("potato");
+            Assert.AreEqual("mice", mice);
+            Assert.AreEqual("potatoes", potatoes);
+        }
+        
+        [TestMethod()]
+        public void ToPlural_ConsonantEnding()
+        {
+            Assert.AreEqual("Tests", "Test".ToPlural());
+
+            var x = PluralizationService.CreateService(CultureInfo.CurrentCulture);
+            string mice = x.Pluralize("mouse");
+            Assert.AreEqual("mice", mice);
+        }
+
+        [TestMethod()]
+        public void ToPlural_Greek()
+        {
+            Assert.AreEqual("Criteria", "Criterion".ToPlural());
+            Assert.AreEqual("phenomena", "phenomenon".ToPlural());
+        }
+
+        //[TestMethod()]
+        //public void ToPlural_Italian()
+        //{
+        //    Assert.AreEqual("virtuoso", "virtuosi".ToPlural());
+        //    Assert.AreEqual("virtuoso", "virtuosos".ToPlural());        //or this?
+        //}
+
+        [TestMethod()]
+        public void ToPlural_Latin()
+        {
+            Assert.AreEqual("Alumnae", "Alumna".ToPlural());
+            Assert.AreEqual("Alumni", "Alumnus".ToPlural());
+            Assert.AreEqual("indices", "index".ToPlural());
+
+            Assert.AreEqual("matrices", "matrix".ToPlural());
+            Assert.AreEqual("stimuli", "stimulus".ToPlural());
+        }
+
+        [TestMethod()]
+        public void ToPlural_OddsAndEnds()
+        {
+            Assert.AreEqual("Lunches", "Lunch".ToPlural());
+            Assert.AreEqual("foxes", "fox".ToPlural());
+            Assert.AreEqual("heroes", "hero".ToPlural());
+            Assert.AreEqual("Torpedoes", "Torpedo".ToPlural());
+        }
+
+        [TestMethod()]
+        public void ReplaceEnd_Test()
+        {
+            string testString = "testString";
+            Assert.AreEqual("testStrTEST", Grammar.ReplaceEnd(testString, 3, "TEST"));
+        }
+    }
+}
